359c3d0aedb85d7c547474b26ccd3bd6dc6cf284
Jeronimo Cardozo <jdcm182@gmail.com>
2022-05-02 23:58:58 -0300

    added Google fonts

--- demo-react/src/App.js
+++ demo-react/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from 'header.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <Header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>';..;'</p>
+      </Header>
     </div>
-  );
+  )
+  /*
+  <div className="App">
+    <header className="App-header">
+      
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+*/
 }
 
 export default App;

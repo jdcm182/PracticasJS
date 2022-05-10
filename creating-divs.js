f8ab28dd8a5b253d7700c641d91543185702932e
Jeronimo Cardozo <jdcm182@gmail.com>
2022-05-09 22:34:05 -0300

    background color randomizer

---
+++ creating-divs.js
@@ -0,0 +1,24 @@
+const grays = ["f8f9fa", "e9ecef", "dee2e6", "ced4da", "adb5bd", "6c757d", "495057", "343a40", "212529"];
+const colors = grays;
+
+/* const wrapper = document.querySelector('#wrapper');
+const addNewBox = function () {
+    const box = document.createElement('div');
+    box.className = 'box';
+    //assign random color
+    let i = Math.random(0, color.length - 1);
+    box.style.backgroundColor = `#${colors[i]}`;
+
+    //box.innerHTML = 
+    wrapper.appendChild(box);
+} */
+
+/* const addButton = document.getElementById('add');
+addButton.addEventListener('click', addNewBox); */
+
+
+let i = //Math.random(0, colors.length - 1);
+    Math.floor(Math.random() * colors.length);
+backgroundColor = `#${colors[i]}`;
+
+
